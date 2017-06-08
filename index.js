aeae3514f9bf0d049e0102386e3ed860fc039210
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-06-08 11:14:40 -0400

    line format
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
 console.log("Hello, kus");
 rts.printMsg = function() {
-  console.log("This is a message from the kush package");
+    console.log("This is a message from the kush package");
 }
-
